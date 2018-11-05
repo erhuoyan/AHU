e6711475a069f342868695c7ffa198ae32b019ef
耿严 <2949702406@qq.com>
2018-11-05 23:18:26 +0800

    .

--- subjects/MOOC/Python语言程序设计（北理）/第三周/e3.4DayDayUp365.py
+++ subjects/MOOC/Python语言程序设计（北理）/第三周/e3.4DayDayUp365.py
@@ -2,7 +2,7 @@
 dayup, dayfactor = 1.0, 0.01
 for i in range(365):
     if i % 7 in [6, 0]:
-        dayup = dayup * (1 + dayfactor)
-    else:
         dayup = dayup * (1 - dayfactor)
+    else:
+        dayup = dayup * (1 + dayfactor)
 print("向上5天向下2天：{:.2f}.".format(dayup))
